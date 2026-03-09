fe16c50d1938567728e43f24778b8b564d70d9bd
cherrevkooя <cherrevkoo@gmail.com>
2026-03-09 22:44:02 +0300

    1-2 hw

--- app/src/main/java/com/practicum/playlist_maker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/SearchActivity.kt
@@ -1,33 +1,20 @@
 package com.practicum.playlist_maker
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        findViewById<MaterialButton>(R.id.search_button).setOnClickListener {
-            startActivity(Intent(this, SearchActivity::class.java))
-        }
-
-        findViewById<MaterialButton>(R.id.media_button).setOnClickListener {
-            startActivity(Intent(this, MediaActivity::class.java))
-        }
-
-        findViewById<MaterialButton>(R.id.settings_button).setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/practicum/playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/SettingsActivity.kt
@@ -1,33 +1,27 @@
 package com.practicum.playlist_maker
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_settings)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        findViewById<MaterialButton>(R.id.search_button).setOnClickListener {
-            startActivity(Intent(this, SearchActivity::class.java))
-        }
 
-        findViewById<MaterialButton>(R.id.media_button).setOnClickListener {
-            startActivity(Intent(this, MediaActivity::class.java))
-        }
-
-        findViewById<MaterialButton>(R.id.settings_button).setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
+        findViewById<ImageButton>(R.id.back_button).setOnClickListener {
+            finish()
         }
     }
 }
