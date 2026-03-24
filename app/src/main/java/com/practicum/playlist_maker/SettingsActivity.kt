f3d42d41353c8a92a05dc39a99141180ff093fab
cherrevkooя <cherrevkoo@gmail.com>
2026-03-24 18:17:33 +0300

    реализованы все конопки экрана настроек

--- app/src/main/java/com/practicum/playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.practicum.playlist_maker
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
+import androidx.core.net.toUri
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -24,5 +28,28 @@ class SettingsActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             finish()
         }
+
+        findViewById<MaterialTextView>(R.id.share_app).setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_course))
+            startActivity(Intent.createChooser(shareIntent, "Поделиться через"))
+        }
+
+        findViewById<MaterialTextView>(R.id.support).setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO,
+                ("mailto:" + getString(R.string.student_email)).toUri())
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_body))
+            startActivity(supportIntent)
+        }
+
+        findViewById<MaterialTextView>(R.id.agreement).setOnClickListener {
+            val agreementIntent = Intent(
+                Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement))
+            )
+            startActivity(agreementIntent)
+        }
+
     }
 }
