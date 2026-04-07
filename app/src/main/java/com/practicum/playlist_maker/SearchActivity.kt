abd7a8fe45f92fb36605efb6a6d98fbf106d0b1c
cherrevkooя <cherrevkoo@gmail.com>
2026-04-07 12:58:03 +0300

    исправлены замечания ревьюера

--- app/src/main/java/com/practicum/playlist_maker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/SearchActivity.kt
@@ -5,17 +5,23 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
     private var searchQuery: String = ""
+
+    private lateinit var searchEditText: EditText
+
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -25,7 +31,7 @@ class SearchActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         searchQuery = savedInstanceState.getString("SEARCH_QUERY", "")
-        findViewById<EditText>(R.id.searchEditText).setText(searchQuery)
+        searchEditText.setText(searchQuery)
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -44,11 +50,7 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val clearIcon = getDrawable(R.drawable.ic_clear)
-
-
-
-        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        searchEditText = findViewById<EditText>(R.id.searchEditText)
 
         searchEditText.post {
             searchEditText.requestFocus()
@@ -56,54 +58,16 @@ class SearchActivity : AppCompatActivity() {
             imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT)
         }
 
-        searchEditText.setOnTouchListener { v, event ->
-            if (event.action == android.view.MotionEvent.ACTION_UP &&
-                searchEditText.compoundDrawablesRelative[2] != null &&
-                event.x >= searchEditText.width - searchEditText.paddingEnd - searchEditText.compoundDrawablesRelative[2].bounds.width()
-                ) {
-                searchEditText.text.clear()
+        val clearButton = findViewById<ImageButton>(R.id.clearButton)
 
-                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-                inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
-
-                true
-            } else false
+        searchEditText.doOnTextChanged { text, _, _, _ ->
+            clearButton.visibility = if (text.isNullOrEmpty()) View.GONE else View.VISIBLE
+            searchQuery = text.toString()
         }
 
-        val clearTextWatcher = object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.isNullOrEmpty()) {
-                    searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                        searchEditText.compoundDrawablesRelative[0],
-                        null,
-                        null,
-                        null
-                    )
-                } else {
-                    val color = if (isNightMode()) {
-                        ContextCompat.getColor(this@SearchActivity, R.color.YP_Black)
-                    } else {
-                        ContextCompat.getColor(this@SearchActivity, R.color.search_line_icon) // #AEAFB4
-                    }
-                    clearIcon?.setTint(color)
-
-                    searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(searchEditText.compoundDrawablesRelative[0],
-                        null, clearIcon,
-                        null )
-                }
-
-                searchQuery = s.toString()
-            }
+        clearButton.setOnClickListener {
+            searchEditText.text.clear()
         }
-
-        searchEditText.addTextChangedListener(clearTextWatcher)
     }
 
     private fun isNightMode(): Boolean {
