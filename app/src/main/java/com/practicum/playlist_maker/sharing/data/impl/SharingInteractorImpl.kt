93fdfabc2bf1eeaef34a9cad04d2244f4aece176
cherrevkooя <cherrevkoo@gmail.com>
2026-07-06 14:29:26 +0300

    изменения по директории settings

--- app/src/main/java/com/practicum/playlist_maker/sharing/data/impl/SharingInteractorImpl.kt
+++ app/src/main/java/com/practicum/playlist_maker/sharing/data/impl/SharingInteractorImpl.kt
@@ -2,31 +2,15 @@ package com.practicum.playlist_maker.sharing.data.impl
 
 import com.practicum.playlist_maker.sharing.domain.ExternalNavigator
 import com.practicum.playlist_maker.sharing.domain.SharingInteractor
+import com.practicum.playlist_maker.sharing.domain.model.EmailData
 
-//class SharingInteractorImpl(
-//    private val externalNavigator: ExternalNavigator,
-//) : SharingInteractor {
-//    override fun shareApp() {
-//        externalNavigator.shareLink(getShareAppLink())
-//    }
-//
-//    override fun openTerms() {
-//        externalNavigator.openLink(getTermsLink())
-//    }
-//
-//    override fun openSupport() {
-//        externalNavigator.openEmail(getSupportEmailData())
-//    }
-//
-//    private fun getShareAppLink(): String {
-//        // Нужно реализовать
-//    }
-//
-//    private fun getSupportEmailData(): EmailData {
-//        // Нужно реализовать
-//    }
-//
-//    private fun getTermsLink(): String {
-//        // Нужно реализовать
-//    }
-//}
+class SharingInteractorImpl(
+    private val externalNavigator: ExternalNavigator,
+) : SharingInteractor {
+
+    override fun shareApp() = externalNavigator.shareApp()
+
+    override fun openTerms() = externalNavigator.openTerms()
+
+    override fun openSupport() = externalNavigator.openSupport()
+}
